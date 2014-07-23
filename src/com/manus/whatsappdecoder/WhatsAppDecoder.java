54d2f379f8782e4ce00b63556c42a4179183487b
Manu Muñoz <mmunoz@tic4you.com>
2014-07-23 11:04:23 +0200

    Removed unnsed inports

--- src/com/manus/whatsappdecoder/WhatsAppDecoder.java
+++ src/com/manus/whatsappdecoder/WhatsAppDecoder.java
@@ -7,16 +7,13 @@ package com.manus.whatsappdecoder;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.apache.commons.lang3.StringEscapeUtils;
 
 /**
  *
